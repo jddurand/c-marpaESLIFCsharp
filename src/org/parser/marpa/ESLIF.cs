3b248dd67e2cc382be33ac090cd912cae66fb534
Jean-Damien Durand <jeandamiendurand@free.fr>
2024-03-15 11:26:29 +0100

    dev+

--- src/org/parser/marpa/ESLIF.cs
+++ src/org/parser/marpa/ESLIF.cs
@@ -2,67 +2,27 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 
 namespace org.parser.marpa
 {
-    public class ESLIF : IDisposable
+    public class ESLIF
     {
         private static readonly object Lock = new object();
         private static readonly Dictionary<IntPtr,ESLIF> Multitons =new Dictionary<IntPtr, ESLIF>();
 
-        private readonly IntPtr marpaESLIFp;
+        public marpaESLIF marpaESLIF { get; }
         private readonly ILogger logger;
-        protected IntPtr genericLoggerp;
-        private GCHandle loggerHandle;
-        private IntPtr loggerHandlePtr;
-        private bool disposedValue;
 
         private ESLIF(ILogger logger)
         {
             this.logger = logger; // Can be null
-
-            // Callback context is a handle to the logger
-            if (this.logger != null)
-            {
-                this.loggerHandle = GCHandle.Alloc(logger, GCHandleType.Normal);
-                this.loggerHandlePtr = GCHandle.ToIntPtr(this.loggerHandle);
-
-                // Log level is driven by logger, so we put GENERICLOGGER_LOGLEVEL_TRACE to filter nothing
-                this.genericLoggerp = genericLoggerShr.genericLogger_newp(LogCallback, this.loggerHandlePtr, genericLoggerShr.genericLoggerLevel_t.GENERICLOGGER_LOGLEVEL_TRACE);
-                if (this.genericLoggerp == IntPtr.Zero)
-                {
-                    DisposeUnmanagedResources();
-                    throw new Exception("genericLogger_newp failure");
-                }
-
-                marpaESLIFShr.marpaESLIFOption_t marpaESLIFOption = new marpaESLIFShr.marpaESLIFOption_t
-                {
-                    genericLoggerp = genericLoggerp,
-                };
-
-                IntPtr marpaESLIFOptionp = Marshal.AllocHGlobal(Marshal.SizeOf(marpaESLIFOption));
-                Marshal.StructureToPtr(marpaESLIFOption, marpaESLIFOptionp, false);
-                this.marpaESLIFp = marpaESLIFShr.marpaESLIF_newp(marpaESLIFOptionp);
-                Marshal.FreeHGlobal(marpaESLIFOptionp);
-            }
-            else
-            {
-                this.marpaESLIFp = marpaESLIFShr.marpaESLIF_newp(IntPtr.Zero);
-            }
-
-            if (this.marpaESLIFp == IntPtr.Zero)
-            {
-                DisposeUnmanagedResources();
-                throw new Exception("marpaESLIF_newp failure");
-            }
+            this.marpaESLIF = new marpaESLIF(new marpaESLIFOption(new genericLogger(logger)));
         }
 
         public static ESLIF Instance(ILogger logger = null)
         {
             lock (Lock)
             {
-                // Multiton key is marpaESLIFp (always != IntPtr.Zero), value is the instance, test is on instance's logger
                 ESLIF ESLIF;
                 KeyValuePair<IntPtr, ESLIF> keyPair = Multitons.FirstOrDefault(p => logger == p.Value.logger);
                 if (keyPair.Key != IntPtr.Zero)
@@ -72,122 +32,31 @@ namespace org.parser.marpa
                 else
                 {
                     ESLIF = new ESLIF(logger);
-                    Multitons.Add(ESLIF.marpaESLIFp, ESLIF);
+                    Multitons.Add(ESLIF.marpaESLIF.marpaESLIFp, ESLIF);
                 }
 
                 return ESLIF;
             }
         }
 
-        private static void LogCallback(IntPtr userDatavp, genericLoggerShr.genericLoggerLevel_t logLeveli, IntPtr msgs)
-        {
-            if (userDatavp == IntPtr.Zero || msgs == IntPtr.Zero)
-            {
-                return;
-            }
-
-            GCHandle loggerHandlePtr = GCHandle.FromIntPtr(userDatavp);
-            ILogger logger = (ILogger) loggerHandlePtr.Target;
-            if (logger == null)
-            {
-                return;
-            }
-
-            string message = Marshal.PtrToStringAnsi(msgs);
-            switch (logLeveli)
-            {
-                case genericLoggerShr.genericLoggerLevel_t.GENERICLOGGER_LOGLEVEL_TRACE:
-                    logger.LogTrace(message);
-                    break;
-                case genericLoggerShr.genericLoggerLevel_t.GENERICLOGGER_LOGLEVEL_DEBUG:
-                    logger.LogDebug(message);
-                    break;
-                case genericLoggerShr.genericLoggerLevel_t.GENERICLOGGER_LOGLEVEL_INFO:
-                    logger.LogInformation(message);
-                    break;
-                case genericLoggerShr.genericLoggerLevel_t.GENERICLOGGER_LOGLEVEL_NOTICE:
-                    logger.LogInformation(message);
-                    break;
-                case genericLoggerShr.genericLoggerLevel_t.GENERICLOGGER_LOGLEVEL_WARNING:
-                    logger.LogWarning(message);
-                    break;
-                case genericLoggerShr.genericLoggerLevel_t.GENERICLOGGER_LOGLEVEL_ALERT:
-                    logger.LogWarning(message);
-                    break;
-                case genericLoggerShr.genericLoggerLevel_t.GENERICLOGGER_LOGLEVEL_EMERGENCY:
-                    logger.LogWarning(message);
-                    break;
-                case genericLoggerShr.genericLoggerLevel_t.GENERICLOGGER_LOGLEVEL_ERROR:
-                    logger.LogError(message);
-                    break;
-                case genericLoggerShr.genericLoggerLevel_t.GENERICLOGGER_LOGLEVEL_CRITICAL:
-                    logger.LogCritical(message);
-                    break;
-            }
-        }
-
-        private void DisposeUnmanagedResources()
-        {
-            if (this.genericLoggerp != IntPtr.Zero)
-            {
-                genericLoggerShr.genericLogger_freev(ref this.genericLoggerp);
-            }
-
-            if (this.loggerHandlePtr != IntPtr.Zero)
-            {
-                this.loggerHandle.Free();
-                this.loggerHandlePtr = IntPtr.Zero;
-            }
-        }
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // DONE: dispose managed state (managed objects)
-                }
-
-                // DONE: free unmanaged resources (unmanaged objects) and override finalizer
-                DisposeUnmanagedResources();
-
-                // DONE: set large fields to null
-                disposedValue = true;
-            }
-        }
-
-        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        ~ESLIF()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: false);
-        }
-
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
-
         public string Version()
         {
-            return marpaESLIF.Version(this.marpaESLIFp);
+            return this.marpaESLIF.Version();
         }
 
         public int VersionMajor()
         {
-            return marpaESLIF.VersionMajor(this.marpaESLIFp);
+            return this.marpaESLIF.VersionMajor();
         }
 
         public int VersionMinor()
         {
-            return marpaESLIF.VersionMinor(this.marpaESLIFp);
+            return this.marpaESLIF.VersionMinor();
         }
 
         public int VersionPatch()
         {
-            return marpaESLIF.VersionPatch(this.marpaESLIFp);
+            return this.marpaESLIF.VersionPatch();
         }
     }
 }
--- src/org/parser/marpa/ESLIFGrammar.cs
+++ src/org/parser/marpa/ESLIFGrammar.cs
@@ -1,5 +1,43 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace org.parser.marpa.dev
+namespace org.parser.marpa
 {
+    public class ESLIFGrammar
+    {
+        private static readonly object Lock = new object();
+        private static readonly Dictionary<IntPtr, ESLIFGrammar> Multitons = new Dictionary<IntPtr, ESLIFGrammar>();
+
+        private readonly marpaESLIFGrammar marpaESLIFGrammar;
+        private readonly ESLIF ESLIF;
+        private readonly string grammar;
+
+        private ESLIFGrammar(ESLIF ESLIF, string grammar)
+        {
+            this.ESLIF = ESLIF ?? throw new ArgumentNullException(nameof(ESLIF));
+            this.grammar = grammar ?? throw new ArgumentNullException(nameof(grammar));
+            this.marpaESLIFGrammar = new marpaESLIFGrammar(this.ESLIF.marpaESLIF, new marpaESLIFGrammarOption(grammar));
+        }
+
+        public static ESLIFGrammar Instance(ESLIF ESLIF, string grammar)
+        {
+            lock (Lock)
+            {
+                ESLIFGrammar ESLIFGrammar;
+                KeyValuePair<IntPtr, ESLIFGrammar> keyPair = Multitons.FirstOrDefault(p => ESLIF == p.Value.ESLIF && grammar == p.Value.grammar);
+                if (keyPair.Key != IntPtr.Zero)
+                {
+                    ESLIFGrammar = keyPair.Value;
+                }
+                else
+                {
+                    ESLIFGrammar = new ESLIFGrammar(ESLIF, grammar);
+                    Multitons.Add(ESLIFGrammar.marpaESLIFGrammar.marpaESLIFGrammarp, ESLIFGrammar);
+                }
+
+                return ESLIFGrammar;
+            }
+        }
+    }
 }
