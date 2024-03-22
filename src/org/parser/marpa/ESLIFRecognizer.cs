dd78a5253b2dbe623c0ffdc8d7eace007d5748f5
Jean-Damien Durand <jeandamiendurand@free.fr>
2024-03-22 09:10:12 +0100

    dev+

---
+++ src/org/parser/marpa/ESLIFRecognizer.cs
@@ -0,0 +1,6 @@
+namespace org.parser.marpa
+{
+    public class ESLIFRecognizer
+    {
+    }
+}
--- src/org/parser/marpa/ESLIFRecognizerInterface.cs
+++ src/org/parser/marpa/ESLIFRecognizerInterface.cs
@@ -2,5 +2,17 @@ namespace org.parser.marpa
 {
     public interface ESLIFRecognizerInterface
     {
+        bool Read();
+        bool IsEof();
+        bool IsCharacterStream();
+        string Encoding();
+        byte[] Data();
+        bool IsWithDisableThreshold();
+        bool IsWithExhaustion();
+        bool IsWithNewline();
+        bool IsWithTrack();
+        void SetESLIFRecognizer(ESLIFRecognizer recognizer);
+        ESLIFRecognizer getESLIFRecognizer();
+
     }
 }
